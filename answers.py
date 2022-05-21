0aebf6042ba3a422fc6354006a7e7c6e385edc31
Elise12 <68713960+Elise12@users.noreply.github.com>
2022-05-21 02:38:49 +0200

    Add files via upload

---
+++ answers.py
@@ -0,0 +1,16 @@
+from config import poderjka
+
+
+#salut  в @Elisehack_bot
+
+a0=f"👨‍🔧Support technique des robots: {poderjka}"
+
+a3="🔑Saisir le code promotionnel"
+
+
+a1='Choisissez la fille que vous aimez💋'
+a11='🏠Vous êtes revenu au menu principal'
+
+a19 = "Nous sommes sur le marché des services sexuels depuis très longtemps\nToutes nos filles sont testées chaque semaine\nSi ​​vous avez des questions, vous pouvez contacter le support technique.\n\nNous faisons de notre mieux pour que nos clients passent une journée inoubliable🔥"
+
+skolkochasov = "Entrez pour combien d'heures vous souhaitez commander un papillon 🧚‍♀️\n\nLorsque vous commandez plus de 2 heures, il y a une réduction de 50 % pour chaque heure suivante @Elisehack_bot"
---
+++ config.py
@@ -0,0 +1,16 @@
+token='1892620363:AAHiAFOeemFUNtz3TwTY4xuz07UDWzt2Q2g:'
+admin=1234
+
+minimalka = 100
+maximalka = 20000
+vxodadmin='Admin' #Mot pour accéder au panneau d'administration (Disponible uniquement pour les administrateurs)
+vxodworker='Travailleur'#Mot pour entrer dans le panneau des travailleurs (Disponible uniquement pour tout le monde)
+zalety = -1234
+bot_username = "@end_raid"
+
+
+poderjka = "@@end_raid" #compte d'assistance
+
+maxpromo = 5000 #le montant maximum de code promotionnel que les travailleurs peuvent créer
+
+
